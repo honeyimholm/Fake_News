7f94e0bd9e4e72ff4f7c666d93d27bd948ce4fbd
TevenLeScao <teven.le-scao@polytechnique.edu>
2018-01-22 18:52:06 -0500

    iteration through the DB

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
 dir = os.path.dirname(__file__)
-DATA_FOLDER = os.path.join(dir, '../Wikidumps')
-RAW_FOLDER = os.path.join(DATA_FOLDER, 'raw/')
+DATA_FOLDER = os.path.join(dir, os.path.join('..', 'Wikidumps'))
+RAW_FOLDER = os.path.join(DATA_FOLDER, 'raw')
 API_KEY = open(os.path.join(RAW_FOLDER, 'API_KEY.txt')).read()
