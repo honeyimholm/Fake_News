8a13eb4bb6fdb807d05a2d8d6cb9a82c0b4dfa9e
TevenLeScao <teven.le-scao@polytechnique.edu>
2018-04-02 18:09:26 -0400

    editor_stats_and_all

--- mapping/extraction/wikichatter_test.py
+++
@@ -1,7 +0,0 @@
-import json
-import wikichatter as wc
-from pprint import pprint
-
-text = open("sample_talk_page.txt").read()
-print(wc.parse(text))
-json.dump(wc.parse(text), open("example.json", 'w'), ensure_ascii=False, indent=2)
